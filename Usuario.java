6a5703d498c0b7bd6fe10fb109e7466b2eb64e50
luishenriquercs <luishenriquee2009@hotmail.com>
2017-02-07 09:50:15 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,30 @@
+package p2cg;
+
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Usuario {
+	
+	private String login;
+	private double dinheiroAtual;
+	HashSet<Jogo> jogosComprados;
+	
+	public Usuario(String login) {
+		this.login = login;
+		this.dinheiroAtual = 0;
+		jogosComprados = new HashSet<Jogo>();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
